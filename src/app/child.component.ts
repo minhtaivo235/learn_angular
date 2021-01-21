709be76484c05589f6dcb35187aa922f9efa19d8
minhtai <minhtaivo235@gmail.com>
2021-01-21 15:23:25 +0700

    demo Output with parameter

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -2,12 +2,17 @@ import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-child',
-    template: `<button (click)="addForParent()">Add</button>`
+    template: `<button (click)="addForParent()">Add</button>
+    <button (click)="subForParent()">Sub</button>
+    `
 })
 export class ChildComponent {
-    @Output() myclick = new EventEmitter();
+    @Output() myclick = new EventEmitter<boolean>();
 
     addForParent() {
-        this.myclick.emit();
+        this.myclick.emit(true);
+    }
+    subForParent() {
+        this.myclick.emit(false);
     }
 }
--- src/app/parent.component.ts
+++ src/app/parent.component.ts
@@ -3,9 +3,15 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-parent',
     template: `<h3>{{value}}</h3>
-        <app-child (myclick)="value = value + 1"></app-child>
+        <app-child (myclick)="changeValue($event)"></app-child>
     `
 })
 export class ParentComponent {
     value = 0;
+    changeValue(status:boolean){
+        if(status)
+        this.value = this.value + 1;
+        else 
+        this.value = this.value - 1;
+    }
 }
